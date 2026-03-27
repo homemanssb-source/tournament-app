6390346806682216e649930d85b23afc2188810e
신승배 <homemanssb@gmail.com>
2026-03-27 10:03:53 +0900

    fix: SW 캐시 버전 v4으로 업데이트

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // JTA 제주시테니스 Service Worker
-const CACHE_NAME = 'jta-ranking-v3';
+const CACHE_NAME = 'jta-ranking-v4';
 
 const STATIC_ASSETS = ['/icon-192x192.png', '/icon-512x512.png', '/manifest.json'];
 
